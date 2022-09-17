86c65f6a9676717bb17e300e99b143b7413687e1
ToQuery <toquery@qq.com>
2022-09-17 22:00:14 +0800

    init

---
+++ src/test/java/io/github/toquery/example/spring/security/jwt/ExampleSpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.toquery.example.spring.security.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
