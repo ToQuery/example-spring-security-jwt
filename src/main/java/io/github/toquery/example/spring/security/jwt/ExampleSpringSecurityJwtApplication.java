87fd5ad83855983f1026c0cf1d949083fb2707c1
ToQuery <toquery@qq.com>
2022-09-20 14:59:22 +0800

    init

--- src/main/java/io/github/toquery/example/spring/security/jwt/ExampleSpringSecurityJwtApplication.java
+++ src/main/java/io/github/toquery/example/spring/security/jwt/ExampleSpringSecurityJwtApplication.java
@@ -1,16 +1,13 @@
 package io.github.toquery.example.spring.security.jwt;
 
-import io.github.toquery.example.spring.security.jwt.properties.OAuthAuthorizationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(value = {OAuthAuthorizationProperties.class})
 public class ExampleSpringSecurityJwtApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExampleSpringSecurityJwtApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleSpringSecurityJwtApplication.class, args);
+    }
 
 }
--- src/main/java/io/github/toquery/example/spring/security/jwt/config/OAuth2ResourceServerSecurityConfiguration.java
+++ src/main/java/io/github/toquery/example/spring/security/jwt/config/OAuth2ResourceServerSecurityConfiguration.java
@@ -1,39 +1,11 @@
-/*
- * Copyright 2020 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package io.github.toquery.example.spring.security.jwt.config;
 
-import com.nimbusds.jose.JWSAlgorithm;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.jwk.source.RemoteJWKSet;
-import com.nimbusds.jose.proc.JWSKeySelector;
-import com.nimbusds.jose.proc.JWSVerificationKeySelector;
-import com.nimbusds.jose.proc.SecurityContext;
-import com.nimbusds.jwt.proc.ConfigurableJWTProcessor;
-import com.nimbusds.jwt.proc.DefaultJWTProcessor;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.net.URL;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -44,9 +16,6 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class OAuth2ResourceServerSecurityConfiguration {
 
-    private final JWSAlgorithm jwsAlgorithm = JWSAlgorithm.RS256;
-    private final OAuth2ResourceServerProperties auth2ResourceServerProperties;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -54,20 +23,8 @@ public class OAuth2ResourceServerSecurityConfiguration {
                         .antMatchers("/message/**").hasAuthority("SCOPE_read")
                         .anyRequest().authenticated()
                 )
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()));
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         return http.build();
-    }
-
-    @SneakyThrows
-    @Bean
-    public JwtDecoder jwtDecoder() {
-        JWKSource<SecurityContext> jwsJwkSource = new RemoteJWKSet<>(new URL(auth2ResourceServerProperties.getJwt().getJwkSetUri()));
-        JWSKeySelector<SecurityContext> jwsKeySelector = new JWSVerificationKeySelector<>(this.jwsAlgorithm, jwsJwkSource);
-
-        ConfigurableJWTProcessor<SecurityContext> jwtProcessor = new DefaultJWTProcessor<>();
-        jwtProcessor.setJWSKeySelector(jwsKeySelector);
-
-        return new NimbusJwtDecoder(jwtProcessor);
     }
 
 
